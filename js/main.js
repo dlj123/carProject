cfbab0306bfc7e2577942e35006a56d4e8cb3a6f
dlj123 <844682619@qq.com>
2018-07-03 18:10:59 +0800

    'add'

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $(function(){
+    
     //小屏上导航菜单的显示和隐藏
     $('#header #menuIcon').on('click',function(){
     
-        if(!$('.nav').hasClass('show')){
+        if($(this).find('i').hasClass('icon-nav')){
+
+            $(this).find('i').removeClass('icon-nav').addClass('icon-cha');
 
            $('.nav').addClass('show');
         }
         else{
+            $(this).find('i').removeClass('icon-cha').addClass('icon-nav');
+
             $('.nav').removeClass('show');
         }
     })
